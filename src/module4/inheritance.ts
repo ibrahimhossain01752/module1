8da5b9b07a774108191eb663711305329dd8f154
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-05-10 23:06:48 +0600

    inhertance

---
+++ src/module4/inheritance.ts
@@ -0,0 +1,29 @@
+class Student {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    makeSleep(hours: number):string{
+        return `This ${this.name} will sleep for ${hours}`
+    }
+}
+
+class Teacher {
+    name: string;
+    age: number;
+    address: string;
+    designation: string;
+
+    constructor(name: string, age: number, address: string, designation: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.designation = designation;
+    }
+}
