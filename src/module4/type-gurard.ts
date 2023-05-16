f3cf251443b0ec92a4c3fc6c828c9c564ed9f9e7
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-05-16 12:57:23 +0600

    in guard

--- src/module4/type-gurard.ts
+++ src/module4/type-gurard.ts
@@ -10,3 +10,17 @@ function add(param1:Alphaneumaric, param2:Alphaneumaric):Alphaneumaric{
 }
 add('1','2')
 add(1,4)
+
+//in gurard
+
+type NormalUserType = {
+    name: string;
+}
+
+type AdminUserType = {
+    name: string;
+    role: "admin";
+}
+
+const normalUser1: NormalUserType = {name:"Kabul Mia"};
+const adminUser1: AdminUserType = {name:"Jabul mia", role:"admin"}
