8ff041899f064f9187b58c3932ccc3a2f6f6d063
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-05-23 14:43:14 +0600

    generic keyof

---
+++ src/conceptual/generic12.ts
@@ -0,0 +1,16 @@
+/* type myArray<T> = Array<T>
+
+
+const myNumber: myArray<number> =[1,3,45,56,65,];
+const myString:myArray<string> =["12","we"];
+const myBoolean:myArray<boolean> =[true,false] */
+
+
+
+type dataType = {name:string, age:number};
+
+const ourInfo:myArray<dataType> =[
+    {name: "name", age:23},
+    {name: "name", age:23},
+    {name: "name", age:23},
+]
